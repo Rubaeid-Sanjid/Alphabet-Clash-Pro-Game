20d988af5e397e50db02990c3e6228f434fceb76
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-16 01:50:46 +0600

    added score and life update functionality

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-function play(){
-    hideElementById('home-screen');
-    showElementById('play-ground');
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  hideElementById("score-board");
+  continueGame();
+}
+
+function continueGame() {
+  const alphabet = generateRandomAlphabet();
+  const currentAlphabet = document.getElementById("screen-alphabet");
+  currentAlphabet.innerText = alphabet;
+
+  setKeyColorById(alphabet);
+}
+
+function handlePressedKey(event) {
+  const playerPressedKey = event.key;
+
+  const currentAlphabetElement = document.getElementById("screen-alphabet");
+  const collectedAlphabet = currentAlphabetElement.innerText;
+  const currentAlphabet = collectedAlphabet.toLowerCase();
+
+  if (playerPressedKey === currentAlphabet) {
+    const scoreElement = document.getElementById("score");
+    const currentScore = scoreElement.innerText;
+    let score = parseInt(currentScore);
+    // const newScore = score + 1;
+    score++;
+
+    // scoreElement.innerText = newScore;
+    scoreElement.innerText = score;
+
+    removeKeyColorById(currentAlphabet);
     continueGame();
-}
+  } else {
+    const lifeElement = document.getElementById("life");
+    const currentLife = lifeElement.innerText;
+    let life = parseInt(currentLife);
 
-function continueGame(){
-    const alphabet = generateRandomAlphabet();
-    const currentAlphabet = document.getElementById('screen-alphabet');
-    currentAlphabet.innerText = alphabet;
+    life--;
+    lifeElement.innerText = life;
 
-    setKeyColorById(alphabet);
-}
-
-
-function handlePressedKey(event){
-    const playerPressedKey = event.key;
-    
-    const currentAlphabetElement = document.getElementById('screen-alphabet');
-    const collectedAlphabet = currentAlphabetElement.innerText;
-    const currentAlphabet = collectedAlphabet.toLowerCase();
-
-    if(playerPressedKey === currentAlphabet){
-        continueGame();
-        removeKeyColorById(currentAlphabet);
+    if (life === 0) {
+      hideElementById("play-ground");
+      showElementById("score-board");
     }
-    else{
-        hideElementById('play-ground');
-        showElementById('score-board');
-    }
-
+  }
 }
-document.addEventListener('keyup', handlePressedKey);
+document.addEventListener("keyup", handlePressedKey);
